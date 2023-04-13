d24b8ebfe9b8c5310e3e6030cbfca97425b9629e
Supriya <59915661+supriya0899@users.noreply.github.com>
2023-04-13 15:55:49 -0700

    Create 441. Arranging Coins.py

---
+++ 441. Arranging Coins.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+
+        l, r= 0, n
+        res = 0
+        while l<=r:
+            mid = (l+r)//2
+
+            coins = (mid/2) *(mid+1)
+            if coins> n:
+                r = mid -1
+            else:
+                l = mid +1
+                res = max(res, mid)
+        return res
